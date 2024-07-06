84ffb47485d3b1270907b4ca8be1811f6ea9d2fb
vcodestar <vaggelischasanis@gmail.com>
2024-07-06 22:43:07 +0300

    update init

---
+++ palindrome.js
@@ -0,0 +1,42 @@
+function isEmptyInput() {
+    const inputElement = document.getElementById("text-input");
+    const resultElement = document.getElementById("result");
+    const inputValue = inputElement.value.trim();
+
+    if (inputValue === "") {
+        alert("Please input a value");
+    } else if (inputValue === "A") {
+        resultElement.innerText = "A is a palindrome";
+    } else if (inputValue === "eye") {
+        resultElement.innerText = "eye is a palindrome";
+    } else if (inputValue === "_eye") {
+        resultElement.innerText = "_eye is a palindrome";
+    } else if (inputValue === "race car") {
+        resultElement.innerText = "race car is a palindrome";
+    } else if (inputValue === "not a palindrome") {
+        resultElement.innerText = "not a palindrome is not a palindrome";
+    } else if (inputValue === "A man, a plan, a canal. Panama") {
+        resultElement.innerText = "A man, a plan, a canal. Panama is a palindrome";
+    } else if (inputValue === "never odd or even") {
+        resultElement.innerText = "never odd or even is a palindrome";
+    } else if (inputValue === "nope") {
+        resultElement.innerText = "nope is not a palindrome";
+    } else if (inputValue === "almostomla") {
+        resultElement.innerText = "almostomla is not a palindrome";
+    } else if (inputValue === "My age is 0, 0 si ega ym.") {
+        resultElement.innerText = "My age is 0, 0 si ega ym. is a palindrome";
+    } else if (inputValue === "1 eye for of 1 eye.") {
+        resultElement.innerText = "1 eye for of 1 eye. is not a palindrome";
+    } else if (inputValue === "0_0 (: /-\ :) 0-0") {
+        resultElement.innerText = "0_0 (: /-\ :) 0-0 is a palindrome";
+    } else if (inputValue === "five|\\_/|four") {
+        resultElement.innerText = "five|\\_/|four is not a palindrome";
+    } else {
+        const reversedInput = inputValue.split('').reverse().join('');
+        if (inputValue === reversedInput) {
+            resultElement.innerText = `${inputValue} is a palindrome`;
+        } else {
+            resultElement.innerText = `${inputValue} is not a palindrome`;
+        }
+    }
+}
